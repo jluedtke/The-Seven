c32eea25f2d0213a701e1836611de1cf9ceeac49
Jared Luedtke <jared7223@hotmail.com>
2017-07-05 19:11:35 -0700

    add shitty movement script for fuckin enemies and players

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,65 +5,57 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-    public float playerSpeed = 4f;
     public LayerMask blockingLayer;
-    public float moveTime = 0.1f;
-
-    private BoxCollider2D boxCollider;
-    private Rigidbody2D rb2D;
-    private float inverseMoveTime;
+    public float moveTime = 1;
 
 
     // Use this for initialization
     void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
-        rb2D = GetComponent<Rigidbody2D>();
-        inverseMoveTime = 1f / moveTime;
-    }
-
-    private void FixedUpdate()
-    {
-        int horizontal = 0;
-        int vertical = 0;
-
-        horizontal = (int)(Input.GetAxisRaw("Horizontal"));
-        vertical = (int)(Input.GetAxisRaw("Vertical"));
-
-        Move(horizontal, vertical);
-
-        //Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        //GetComponent<Rigidbody2D>().velocity = targetVelocity * playerSpeed;
-
+        InvokeRepeating("AttemptMove", 0f, .1f); //change or find a better way
 
     }
 
-    private bool Move(int xDir, int yDir)
+    private void AttemptMove()
     {
-        RaycastHit2D hit;
-        Vector2 start = transform.position;
-        Vector2 end = start + new Vector2(xDir, yDir);
-        hit = Physics2D.Linecast(start, end, blockingLayer);
-        if (hit.transform == null)
+        if (GameManager.instance.playerTurn == false)
         {
-            StartCoroutine(SmoothMovement(end));
-            return true;
+            return;
         }
-        return false;
+
+        Vector3 targetVelocity = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), -1);
+
+        if (targetVelocity.x == 0 && targetVelocity.y == 0)
+        {
+            return;
+        }
+        else
+            Move(targetVelocity);
+
+
     }
 
-
-    private IEnumerator SmoothMovement(Vector3 end)
+    private void Move(Vector3 targetVelocity)
     {
-        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-
-        while (sqrRemainingDistance > float.Epsilon)
+        Debug.Log(targetVelocity);
+        if (GameManager.instance.playerTurn == false)
         {
-            Vector3 newPosition = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
-            rb2D.MovePosition(newPosition);
-            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-
-            yield return null;
+            return;
         }
+        else
+        {
+            GameManager.instance.playerTurn = false;
+
+            Vector3 position = this.transform.position;
+            position.x += targetVelocity.x;
+            position.y += targetVelocity.y;
+
+            this.transform.position = position;
+
+        }
+
+    // if (Object.instance.playerTurn = false) { playerTurn = true; return; }
+    // Move once per turn. Or a certain amount of spaces defined in Player Object
+    // Object.instance.playerTurn = false;
     }
 }
---
+++ Assets/Scripts/Unused/Enemy.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float health = 10f;
+    public float damage = 1f;
+    public float attackRange = 1f;
+    public float moveRange = 1f;
+
+    private Transform target;
+
+    public LayerMask blockingLayer;
+    public float moveTime = 1;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        InvokeRepeating("AttemptMove", 0f, .1f); // change or find a better way
+    }
+
+    private void AttemptMove()
+    {
+        if (GameManager.instance.playerTurn == true)
+        {
+            return;
+        }
+
+        int xDir = 0;
+        int yDir = 0;
+
+        if (Mathf.Abs(target.position.x - transform.position.x) < .1f)
+        {
+            yDir = target.position.y > transform.position.y ? 1 : -1;
+        }
+        else
+        {
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+        }
+
+        Vector3 targetVelocity = new Vector3(xDir, yDir, -1);
+
+
+        Move(targetVelocity);
+
+
+    }
+
+    private void Move(Vector3 targetVelocity)
+    {
+
+        if (GameManager.instance.playerTurn == true)
+        {
+            return;
+        }
+        else
+        {
+            GameManager.instance.playerTurn = true;
+
+            Vector3 position = this.transform.position;
+            position.x += targetVelocity.x;
+            position.y += targetVelocity.y;
+
+            this.transform.position = position;
+
+        }
+    }
+}
